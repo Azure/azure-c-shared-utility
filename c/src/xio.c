9cf42af3be5ff098cba01fe71861f69350597de5
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2015-11-24 17:09:50 -0800

    Rename io to xio in the xio requierements doc

--- c/src/xio.c
+++ c/src/xio.c
@@ -18,9 +18,9 @@ typedef struct XIO_INSTANCE_TAG
 XIO_HANDLE xio_create(const IO_INTERFACE_DESCRIPTION* io_interface_description, const void* xio_create_parameters, LOGGER_LOG logger_log)
 {
 	XIO_INSTANCE* xio_instance;
-	/* Codes_SRS_IO_01_003: [If the argument io_interface_description is NULL, xio_create shall return NULL.] */
+	/* Codes_SRS_XIO_01_003: [If the argument io_interface_description is NULL, xio_create shall return NULL.] */
 	if ((io_interface_description == NULL) ||
-		/* Codes_SRS_IO_01_004: [If any io_interface_description member is NULL, xio_create shall return NULL.] */
+		/* Codes_SRS_XIO_01_004: [If any io_interface_description member is NULL, xio_create shall return NULL.] */
 		(io_interface_description->concrete_io_create == NULL) ||
 		(io_interface_description->concrete_io_destroy == NULL) ||
 		(io_interface_description->concrete_io_open == NULL) ||
@@ -34,16 +34,16 @@ XIO_HANDLE xio_create(const IO_INTERFACE_DESCRIPTION* io_interface_description, 
 	{
 		xio_instance = (XIO_INSTANCE*)malloc(sizeof(XIO_INSTANCE));
 
-		/* Codes_SRS_IO_01_017: [If allocating the memory needed for the IO interface fails then xio_create shall return NULL.] */
+		/* Codes_SRS_XIO_01_017: [If allocating the memory needed for the IO interface fails then xio_create shall return NULL.] */
 		if (xio_instance != NULL)
 		{
-			/* Codes_SRS_IO_01_001: [xio_create shall return on success a non-NULL handle to a new IO interface.] */
+			/* Codes_SRS_XIO_01_001: [xio_create shall return on success a non-NULL handle to a new IO interface.] */
 			xio_instance->io_interface_description = io_interface_description;
 
-			/* Codes_SRS_IO_01_002: [In order to instantiate the concrete IO implementation the function concrete_io_create from the io_interface_description shall be called, passing the xio_create_parameters and logger_log arguments.] */
+			/* Codes_SRS_XIO_01_002: [In order to instantiate the concrete IO implementation the function concrete_io_create from the io_interface_description shall be called, passing the xio_create_parameters and logger_log arguments.] */
 			xio_instance->concrete_xio_handle = xio_instance->io_interface_description->concrete_io_create((void*)xio_create_parameters, logger_log);
 
-			/* Codes_SRS_IO_01_016: [If the underlying concrete_io_create call fails, xio_create shall return NULL.] */
+			/* Codes_SRS_XIO_01_016: [If the underlying concrete_io_create call fails, xio_create shall return NULL.] */
 			if (xio_instance->concrete_xio_handle == NULL)
 			{
 				free(xio_instance);
@@ -56,15 +56,15 @@ XIO_HANDLE xio_create(const IO_INTERFACE_DESCRIPTION* io_interface_description, 
 
 void xio_destroy(XIO_HANDLE xio)
 {
-	/* Codes_SRS_IO_01_007: [If the argument io is NULL, xio_destroy shall do nothing.] */
+	/* Codes_SRS_XIO_01_007: [If the argument io is NULL, xio_destroy shall do nothing.] */
 	if (xio != NULL)
 	{
 		XIO_INSTANCE* xio_instance = (XIO_INSTANCE*)xio;
 
-		/* Codes_SRS_IO_01_006: [xio_destroy shall also call the concrete_io_destroy function that is member of the io_interface_description argument passed to xio_create, while passing as argument to concrete_io_destroy the result of the underlying concrete_io_create handle that was called as part of the xio_create call.] */
+		/* Codes_SRS_XIO_01_006: [xio_destroy shall also call the concrete_io_destroy function that is member of the io_interface_description argument passed to xio_create, while passing as argument to concrete_io_destroy the result of the underlying concrete_io_create handle that was called as part of the xio_create call.] */
 		xio_instance->io_interface_description->concrete_io_destroy(xio_instance->concrete_xio_handle);
 
-		/* Codes_SRS_IO_01_005: [xio_destroy shall free all resources associated with the IO handle.] */
+		/* Codes_SRS_XIO_01_005: [xio_destroy shall free all resources associated with the IO handle.] */
 		free(xio_instance);
 	}
 }
@@ -75,22 +75,22 @@ int xio_open(XIO_HANDLE xio, ON_BYTES_RECEIVED on_bytes_received, ON_IO_STATE_CH
 
 	if (xio == NULL)
 	{
-		/* Codes_SRS_IO_01_021: [If handle is NULL, xio_open shall return a non-zero value.] */
+		/* Codes_SRS_XIO_01_021: [If handle is NULL, xio_open shall return a non-zero value.] */
 		result = __LINE__;
 	}
 	else
 	{
 		XIO_INSTANCE* xio_instance = (XIO_INSTANCE*)xio;
 
-		/* Codes_SRS_IO_01_019: [xio_open shall call the specific concrete_io_open function specified in xio_create, passing the receive_callback and receive_callback_context arguments.] */
+		/* Codes_SRS_XIO_01_019: [xio_open shall call the specific concrete_io_open function specified in xio_create, passing the receive_callback and receive_callback_context arguments.] */
 		if (xio_instance->io_interface_description->concrete_io_open(xio_instance->concrete_xio_handle, on_bytes_received, on_xio_state_changed, callback_context) != 0)
 		{
-			/* Codes_SRS_IO_01_022: [If the underlying concrete_io_open fails, xio_open shall return a non-zero value.] */
+			/* Codes_SRS_XIO_01_022: [If the underlying concrete_io_open fails, xio_open shall return a non-zero value.] */
 			result = __LINE__;
 		}
 		else
 		{
-			/* Codes_SRS_IO_01_020: [On success, xio_open shall return 0.] */
+			/* Codes_SRS_XIO_01_020: [On success, xio_open shall return 0.] */
 			result = 0;
 		}
 	}
@@ -104,22 +104,22 @@ int xio_close(XIO_HANDLE xio)
 
 	if (xio == NULL)
 	{
-		/* Codes_SRS_IO_01_025: [If handle is NULL, xio_close shall return a non-zero value.] */
+		/* Codes_SRS_XIO_01_025: [If handle is NULL, xio_close shall return a non-zero value.] */
 		result = __LINE__;
 	}
 	else
 	{
 		XIO_INSTANCE* xio_instance = (XIO_INSTANCE*)xio;
 
-		/* Codes_SRS_IO_01_023: [xio_close shall call the specific concrete_io_close function specified in xio_create.] */
+		/* Codes_SRS_XIO_01_023: [xio_close shall call the specific concrete_io_close function specified in xio_create.] */
 		if (xio_instance->io_interface_description->concrete_io_close(xio_instance->concrete_xio_handle) != 0)
 		{
-			/* Codes_SRS_IO_01_026: [If the underlying concrete_io_close fails, xio_close shall return a non-zero value.] */
+			/* Codes_SRS_XIO_01_026: [If the underlying concrete_io_close fails, xio_close shall return a non-zero value.] */
 			result = __LINE__;
 		}
 		else
 		{
-			/* Codes_SRS_IO_01_024: [On success, xio_close shall return 0.] */
+			/* Codes_SRS_XIO_01_024: [On success, xio_close shall return 0.] */
 			result = 0;
 		}
 	}
@@ -131,8 +131,8 @@ int xio_send(XIO_HANDLE xio, const void* buffer, size_t size, ON_SEND_COMPLETE o
 {
 	int result;
 
-	/* Codes_SRS_IO_01_011: [No error check shall be performed on buffer and size.] */
-	/* Codes_SRS_IO_01_010: [If handle is NULL, xio_send shall return a non-zero value.] */
+	/* Codes_SRS_XIO_01_011: [No error check shall be performed on buffer and size.] */
+	/* Codes_SRS_XIO_01_010: [If handle is NULL, xio_send shall return a non-zero value.] */
 	if (xio == NULL)
 	{
 		result = __LINE__;
@@ -141,10 +141,10 @@ int xio_send(XIO_HANDLE xio, const void* buffer, size_t size, ON_SEND_COMPLETE o
 	{
 		XIO_INSTANCE* xio_instance = (XIO_INSTANCE*)xio;
 
-		/* Codes_SRS_IO_01_008: [xio_send shall pass the sequence of bytes pointed to by buffer to the concrete IO implementation specified in xio_create, by calling the concrete_io_send function while passing down the buffer and size arguments to it.] */
-		/* Codes_SRS_IO_01_009: [On success, xio_send shall return 0.] */
-		/* Codes_SRS_IO_01_015: [If the underlying concrete_io_send fails, xio_send shall return a non-zero value.] */
-		/* Codes_SRS_IO_01_027: [xio_send shall pass to the concrete_io_send function the on_send_complete and callback_context arguments.] */
+		/* Codes_SRS_XIO_01_008: [xio_send shall pass the sequence of bytes pointed to by buffer to the concrete IO implementation specified in xio_create, by calling the concrete_io_send function while passing down the buffer and size arguments to it.] */
+		/* Codes_SRS_XIO_01_009: [On success, xio_send shall return 0.] */
+		/* Codes_SRS_XIO_01_015: [If the underlying concrete_io_send fails, xio_send shall return a non-zero value.] */
+		/* Codes_SRS_XIO_01_027: [xio_send shall pass to the concrete_io_send function the on_send_complete and callback_context arguments.] */
 		result = xio_instance->io_interface_description->concrete_io_send(xio_instance->concrete_xio_handle, buffer, size, on_send_complete, callback_context);
 	}
 
@@ -153,12 +153,12 @@ int xio_send(XIO_HANDLE xio, const void* buffer, size_t size, ON_SEND_COMPLETE o
 
 void xio_dowork(XIO_HANDLE xio)
 {
-	/* Codes_SRS_IO_01_018: [When the handle argument is NULL, xio_dowork shall do nothing.] */
+	/* Codes_SRS_XIO_01_018: [When the handle argument is NULL, xio_dowork shall do nothing.] */
 	if (xio != NULL)
 	{
 		XIO_INSTANCE* xio_instance = (XIO_INSTANCE*)xio;
 
-		/* Codes_SRS_IO_01_012: [xio_dowork shall call the concrete XIO implementation specified in xio_create, by calling the concrete_io_dowork function.] */
+		/* Codes_SRS_XIO_01_012: [xio_dowork shall call the concrete XIO implementation specified in xio_create, by calling the concrete_io_dowork function.] */
 		xio_instance->io_interface_description->concrete_io_dowork(xio_instance->concrete_xio_handle);
 	}
 }
