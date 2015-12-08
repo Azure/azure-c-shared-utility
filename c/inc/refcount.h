b71f48de03d4018f4292cd051fbda1fe37bb8fbc
anporumb <anporumb@microsoft.com>
2015-12-08 10:42:40 -0800

    after CR

--- c/inc/refcount.h
+++ c/inc/refcount.h
@@ -2,8 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 
-/*this header contains macros for ref_couting an immutable variable. The immutability is not ensured by
-this header, only the REFCOUNTing.
+/*this header contains macros for ref_counting a variable. 
 
 There are no upper bound checks related to uint32_t overflow because we expect that bigger issues are in
 the system when more than 4 billion references exist to the same variable. In the case when such an overflow
@@ -34,29 +33,29 @@ struct C2(C2(REFCOUNT_, type), _TAG)
 #define REFCOUNT_SHORT_TYPE(type) \
 C2(REFCOUNT_, type)
 
-#define REFCOUNT_TYPE_CREATE_TYPE(type) C2(REFCOUNT_SHORT_TYPE(type), _Create)
+#define REFCOUNT_TYPE_DECLARE_CREATE(type) C2(REFCOUNT_SHORT_TYPE(type), _Create)
 #define REFCOUNT_TYPE_CREATE(type) C2(REFCOUNT_SHORT_TYPE(type), _Create)()
 
-/*this introduces a new refcount'd type based on another type
-and an initializer for that new type that also sets the ref count to 1. The type must not have a flexible array*/
+/*this introduces a new refcount'd type based on another type */
+/*and an initializer for that new type that also sets the ref count to 1. The type must not have a flexible array*/
 /*the newly allocated memory shall be free'd by free()*/
-/*and the ref counting is handled internally by the type in teh _Create/ _Clone /_Destroy functions */
+/*and the ref counting is handled internally by the type in the _Create/ _Clone /_Destroy functions */
 
-#define DEFINE_REFCOUNT_TYPE(type)                                             \
-REFCOUNT_TYPE(type)                                                            \
-{                                                                              \
-    type counted;                                                              \
-    uint32_t count;                                                          \
-};                                                                             \
-static type* REFCOUNT_TYPE_CREATE_TYPE(type) (void)                            \
-{                                                                              \
+#define DEFINE_REFCOUNT_TYPE(type)                                                                   \
+REFCOUNT_TYPE(type)                                                                                  \
+{                                                                                                    \
+    type counted;                                                                                    \
+    uint32_t count;                                                                                  \
+};                                                                                                   \
+static type* REFCOUNT_TYPE_DECLARE_CREATE(type) (void)                                               \
+{                                                                                                    \
     REFCOUNT_TYPE(type)* result = (REFCOUNT_TYPE(type)*)malloc(sizeof(REFCOUNT_TYPE(type)));         \
-    if (result != NULL)                                                        \
-    {                                                                          \
-        result->count = 1;                                                     \
-    }                                                                          \
-    return (type*)result;                                                      \
-}                                                                              \
+    if (result != NULL)                                                                              \
+    {                                                                                                \
+        result->count = 1;                                                                           \
+    }                                                                                                \
+    return (type*)result;                                                                            \
+}                                                                                                    \
 
 /*the following macros increment/decrement a ref count in an atomic way, depending on the platform*/
 /*The following mechanisms are considered in this order
@@ -72,11 +71,16 @@ gcc
     - will result in no include (for gcc these are intrinsics build in)
     - will use __sync_fetch_and_add/sub
     - about the return value: "... returns the value that had previously been in memory." (https://gcc.gnu.org/onlinedocs/gcc-4.4.3/gcc/Atomic-Builtins.html#Atomic-Builtins)
-
-It seems windows is "one off" because it returns the value "before" the decrement, as opposed to C11 standard and gcc that return the value "before". 
+other cases
+    - if REFCOUNT_ATOMIC_DONTCARE is defined, then 
+        will result in ++/-- used for increment/decrement.
+    - if it is not defined, then error out
+       
+It seems windows is "one off" because it returns the value "after" the decrement, as opposed to C11 standard and gcc that return the value "before". 
 The macro DEC_RETURN_ZERO will be "0" on windows, and "1" on the other cases.
 */
 
+/*if macro DEC_REF returns DEC_RETURN_ZERO that means the ref count has reached zero.*/
 #if defined(__STDC_VERSION__) && (__STDC_VERSION__ == 201112) && (__STDC_NO_ATOMICS__!=1)
 #include <stdatomic.h>
 #define DEC_RETURN_ZERO (1)
@@ -95,7 +99,13 @@ The macro DEC_RETURN_ZERO will be "0" on windows, and "1" on the other cases.
 #define DEC_REF(type, var) __sync_fetch_and_sub((&((REFCOUNT_TYPE(type)*)var)->count), 1)
 
 #else
+#if defined(REFCOUNT_ATOMIC_DONTCARE)
+#define DEC_RETURN_ZERO (0)
+#define INC_REF(type, var) ++(&(((REFCOUNT_TYPE(type)*)var)->count))
+#define DEC_REF(type, var) --(&(((REFCOUNT_TYPE(type)*)var)->count))
+#else
 #error do not know how to atomically increment and decrement a uint32_t :(. Platform support needs to be extended to your platform.
+#endif /*defined(REFCOUNT_ATOMIC_DONTCARE)*/
 #endif
 
 
