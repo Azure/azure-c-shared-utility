e3c4f7e81fedd0fb2b477ced0ed9b39c30364e3b
Jelani <jelani.brandon@microsoft.com>
2016-02-04 14:58:12 -0800

    Update Nuget package

--- c/build_all/packaging/windows/rebuild_nugets.cmd
+++ c/build_all/packaging/windows/rebuild_nugets.cmd
@@ -4,14 +4,22 @@
 @setlocal EnableExtensions EnableDelayedExpansion
 @echo off
 
-rem - Specify the Azure SDK client build root
-set current-path=%~dp0
-set current-path=%current-path:~0,-1%
-set client-root=%current-path%\..\..\..\..
-for %%i in ("%client-root%") do set client-root=%%~fi
-echo Client root is %client-root%
+rem -----------------------------------------------------------------------------
+rem -- setup path information
+rem -----------------------------------------------------------------------------
 
-pushd %client-root%\c\build_all\packaging\windows\
+set current-path=%~dp0
+rem // remove trailing slash
+set current-path=%current-path:~0,-1%
+
+echo Current Path: %current-path%
+
+set build-root=%current-path%\..\..\..
+rem // resolve to fully qualified path
+for %%i in ("%build-root%") do set build-root=%%~fi
+
+set client-root=%current-path%\..\..\..
+for %%i in ("%client-root%") do set client-root=%%~fi
 
 where /q nuget.exe
 if not !errorlevel! == 0 (
@@ -27,27 +35,102 @@ Powershell.exe wget -outf nuget.exe https://nuget.org/nuget.exe
 	)
 )
 
+if exist %USERPROFILE%\shared-util_nuget (
+	rmdir /s/q %USERPROFILE%\shared-util_nuget
+	rem no error checking
+)
+
+if exist %client-root%\shared-util_output (
+	rmdir /s/q %client-root%\shared-util_output
+	rem no error checking
+)
+
 del *.nupkg
 
+rem -----------------------------------------------------------------------------
+rem -- build with CMAKE
+rem -----------------------------------------------------------------------------
+
+echo Build root is %build-root%
+echo Client root is %client-root%
+
+mkdir %USERPROFILE%\shared-util_nuget
+rem no error checking
+
+pushd %USERPROFILE%\shared-util_nuget
+
+rem Build Win32
+cmake %build-root%
+if not %errorlevel%==0 exit /b %errorlevel%
+
+call :_run-msbuild "Build" azure_c_shared_utility.sln Debug Win32
+if not %errorlevel%==0 exit /b %errorlevel%
+
 rem -- Copy all Win32 files from cmake build directory to the repo directory
-xcopy /q /y /R %USERPROFILE%\shared-util_Win32\Debug\*.* %client-root%\shared-util\c\win32\debug\*.*
+xcopy /q /y /R %USERPROFILE%\shared-util_nuget\Debug\*.* %client-root%\shared-util_output\win32\debug\*.*
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+call :_run-msbuild "Build" azure_c_shared_utility.sln Release Win32
+if not %errorlevel%==0 exit /b %errorlevel%
 
 rem -- Copy all Win32 Release files from cmake build directory to the repo directory
-xcopy /q /y /R %USERPROFILE%\shared-util_Win32\Release\*.* %client-root%\shared-util\c\win32\Release\*.*
+xcopy /q /y /R %USERPROFILE%\shared-util_nuget\Release\*.* %client-root%\shared-util_output\win32\Release\*.*
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+rem -- Remove the x86 cmake files
+rmdir /s/q %USERPROFILE%\shared-util_nuget
+rem no error checking
+
+rem -----------------------------------------------------------------------------
+rem -- build with CMAKE x64
+rem -----------------------------------------------------------------------------
+
+cmake %build-root% -G "Visual Studio 14 Win64"
+if not %errorlevel%==0 exit /b %errorlevel%
+
+call :_run-msbuild "Build" azure_c_shared_utility.sln Debug x64
+if not %errorlevel%==0 exit /b %errorlevel%
 
 rem -- Copy all x64 files from cmake build directory to the repo directory
-xcopy /q /y /R %USERPROFILE%\shared-util_x64\Debug\*.* %client-root%\shared-util\c\x64\debug\*.*
+xcopy /q /y /R %USERPROFILE%\shared-util_nuget\Debug\*.* %client-root%\shared-util_output\x64\debug\*.*
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+call :_run-msbuild "Build" azure_c_shared_utility.sln Release x64
+if not %errorlevel%==0 exit /b %errorlevel%
+
 rem -- Copy all x64 Release files from cmake build directory to the repo directory
-xcopy /q /y /R %USERPROFILE%\shared-util_x64\Release\*.* %client-root%\shared-util\c\x64\Release\*.*
+xcopy /q /y /R %USERPROFILE%\shared-util_nuget\Release\*.* %client-root%\shared-util_output\x64\Release\*.*
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+if exist *.nupkg (
+	del *.nupkg
+)
+
+popd
 
 rem -- Package Nuget
 nuget pack Microsoft.Azure.C.SharedUtility.nuspec
 
-rmdir %client-root%\shared-util /S /Q
+rmdir %client-root%\shared-util_output /S /Q
+rmdir %USERPROFILE%\shared-util_nuget /S /Q
 
 popd
+goto :eof
+
+rem -----------------------------------------------------------------------------
+rem -- helper subroutines
+rem -----------------------------------------------------------------------------
+
+:_run-msbuild
+rem // optionally override configuration|platform
+setlocal EnableExtensions
+set build-target=
+if "%~1" neq "Build" set "build-target=/t:%~1"
+if "%~3" neq "" set build-config=%~3
+if "%~4" neq "" set build-platform=%~4
+
+msbuild /m %build-target% "/p:Configuration=%build-config%;Platform=%build-platform%" %2
+if not %errorlevel%==0 exit /b %errorlevel%
+goto :eof
+
+echo done
