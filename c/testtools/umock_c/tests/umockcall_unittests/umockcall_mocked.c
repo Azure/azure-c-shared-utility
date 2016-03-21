71271fbb8e2171fb591a5b510e7a2b2e3160df64
dcristoloveanu <dcristo@microsoft.com>
2016-03-21 14:10:29 -0700

    Fix warnings on Windows with crtdbg

--- c/testtools/umock_c/tests/umockcall_unittests/umockcall_mocked.c
+++ c/testtools/umock_c/tests/umockcall_unittests/umockcall_mocked.c
@@ -12,10 +12,17 @@ extern void* mock_calloc(size_t nmemb, size_t size);
 extern void* mock_realloc(void* ptr, size_t size);
 extern void mock_free(void* ptr);
 
+#ifdef _CRTDBG_MAP_ALLOC
+#define _malloc_dbg(size, ...) mock_malloc(size)
+#define _calloc_dbg(nmemb, size, ...) mock_calloc(nmemb, size)
+#define _realloc_dbg(ptr, size, ...) mock_realloc(ptr, size)
+#define _free_dbg(ptr, ...) mock_free(ptr)
+#else
 #define malloc(size) mock_malloc(size)
 #define calloc(nmemb, size) mock_calloc(nmemb, size)
 #define realloc(ptr, size) mock_realloc(ptr, size)
 #define free(ptr) mock_free(ptr)
+#endif
 
 /* include code under test */
 #include "../../src/umockcall.c"
