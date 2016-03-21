d399728bb8bbdea294fab25760fcf80345be6391
Dan Cristoloveanu <dcristoloveanu@yahoo.com>
2016-03-20 22:21:01 -0700

    Add some tests for umock_c

--- c/testtools/umock_c/src/umock_c.c
+++ c/testtools/umock_c/src/umock_c.c
@@ -12,6 +12,12 @@
 #include "umocktypes.h"
 #include "umocktypes_c.h"
 
+typedef enum UMOCK_C_STATE_TAG
+{
+    UMOCK_C_STATE_NOT_INITIALIZED,
+    UMOCK_C_STATE_INITIALIZED
+} UMOCK_C_STATE;
+
 static size_t expected_call_count;
 static UMOCKCALL_HANDLE* expected_calls;
 static size_t actual_call_count;
@@ -19,35 +25,44 @@ static UMOCKCALL_HANDLE* actual_calls;
 static char* expected_calls_string;
 static char* actual_calls_string;
 static ON_UMOCK_C_ERROR on_umock_c_error_function;
+static UMOCK_C_STATE umock_c_state = UMOCK_C_STATE_NOT_INITIALIZED;
 
 int umock_c_init(ON_UMOCK_C_ERROR on_umock_c_error)
 {
     int result;
 
-    /* Codes_SRS_UMOCK_C_01_144: [ Out of the box umock_c shall support the following types through the header umocktypes_c.h: ]*/
-    /* Codes_SRS_UMOCK_C_01_028: [**char**] */
-    /* Codes_SRS_UMOCK_C_01_029 : [**unsigned char**] */
-    /* Codes_SRS_UMOCK_C_01_030 : [**short**] */
-    /* Codes_SRS_UMOCK_C_01_031 : [**unsigned short**] */
-    /* Codes_SRS_UMOCK_C_01_032 : [**int**] */
-    /* Codes_SRS_UMOCK_C_01_033 : [**unsigned int**] */
-    /* Codes_SRS_UMOCK_C_01_034 : [**long**] */
-    /* Codes_SRS_UMOCK_C_01_035 : [**unsigned long**] */
-    /* Codes_SRS_UMOCK_C_01_036 : [**long long**] */
-    /* Codes_SRS_UMOCK_C_01_037 : [**unsigned long long**] */
-    /* Codes_SRS_UMOCK_C_01_038 : [**float**] */
-    /* Codes_SRS_UMOCK_C_01_039 : [**double**] */
-    /* Codes_SRS_UMOCK_C_01_040 : [**long double**] */
-    /* Codes_SRS_UMOCK_C_01_041 : [**size_t**] */
-    if ((umocktypes_init() != 0) ||
-        (umocktypes_c_register_types() != 0))
+    if (umock_c_state != UMOCK_C_STATE_NOT_INITIALIZED)
     {
         result = __LINE__;
     }
-    else 
+    else
     {
-        on_umock_c_error_function = on_umock_c_error;
-        result = 0;
+        /* Codes_SRS_UMOCK_C_01_144: [ Out of the box umock_c shall support the following types through the header umocktypes_c.h: ]*/
+        /* Codes_SRS_UMOCK_C_01_028: [**char**] */
+        /* Codes_SRS_UMOCK_C_01_029 : [**unsigned char**] */
+        /* Codes_SRS_UMOCK_C_01_030 : [**short**] */
+        /* Codes_SRS_UMOCK_C_01_031 : [**unsigned short**] */
+        /* Codes_SRS_UMOCK_C_01_032 : [**int**] */
+        /* Codes_SRS_UMOCK_C_01_033 : [**unsigned int**] */
+        /* Codes_SRS_UMOCK_C_01_034 : [**long**] */
+        /* Codes_SRS_UMOCK_C_01_035 : [**unsigned long**] */
+        /* Codes_SRS_UMOCK_C_01_036 : [**long long**] */
+        /* Codes_SRS_UMOCK_C_01_037 : [**unsigned long long**] */
+        /* Codes_SRS_UMOCK_C_01_038 : [**float**] */
+        /* Codes_SRS_UMOCK_C_01_039 : [**double**] */
+        /* Codes_SRS_UMOCK_C_01_040 : [**long double**] */
+        /* Codes_SRS_UMOCK_C_01_041 : [**size_t**] */
+        if ((umocktypes_init() != 0) ||
+            (umocktypes_c_register_types() != 0))
+        {
+            result = __LINE__;
+        }
+        else
+        {
+            on_umock_c_error_function = on_umock_c_error;
+            umock_c_state = UMOCK_C_STATE_INITIALIZED;
+            result = 0;
+        }
     }
 
     return result;
@@ -55,13 +70,19 @@ int umock_c_init(ON_UMOCK_C_ERROR on_umock_c_error)
 
 void umock_c_deinit(void)
 {
-    umock_c_reset_all_calls();
+    /* Codes_SRS_UMOCK_C_01_012: [If umock_c was not initialized, umock_c_deinit shall do nothing.] */
+    if (umock_c_state == UMOCK_C_STATE_INITIALIZED)
+    {
+        (void)umock_c_reset_all_calls();
 
-    free(actual_calls_string);
-    actual_calls_string = NULL;
-    free(expected_calls_string);
-    expected_calls_string = NULL;
-    umocktypes_deinit();
+        free(actual_calls_string);
+        actual_calls_string = NULL;
+        free(expected_calls_string);
+        expected_calls_string = NULL;
+        umocktypes_deinit();
+
+        umock_c_state = UMOCK_C_STATE_NOT_INITIALIZED;
+    }
 }
 
 void umock_c_indicate_error(UMOCK_C_ERROR_CODE error_code)
@@ -133,7 +154,6 @@ int umock_c_add_actual_call(UMOCKCALL_HANDLE mock_call, UMOCKCALL_HANDLE* matche
     for (i = 0; i < expected_call_count; i++)
     {
         int are_equal_result = umockcall_are_equal(expected_calls[i], mock_call);
-
         if (are_equal_result == 1)
         {
             *matched_call = expected_calls[i];
