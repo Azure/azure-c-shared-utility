89f9a752dd23ef8b4eb852505d64e0829764b2b9
dcristoloveanu <dcristo@microsoft.com>
2016-04-24 22:09:45 -0700

    Remove warnings in umock_log.h

--- testtools/umock_c/inc/umock_log.h
+++ testtools/umock_c/inc/umock_log.h
@@ -10,7 +10,12 @@
 #include <stdio.h>
 #endif
 
+#if defined _MSC_VER
 #define UMOCK_LOG(format, ...) \
     (void)printf(format "\r\n", __VA_ARGS__);
+#else
+#define UMOCK_LOG(format, ...) \
+    (void)printf(format "\r\n", ##__VA_ARGS__);
+#endif
 
 #endif /* UMOCK_LOG_H */
