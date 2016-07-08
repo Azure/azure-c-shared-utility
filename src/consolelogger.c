4864d1b315c43aa19f8297cea40bd76916c1447c
mamokarz <mamokarz@microsoft.com>
2016-07-08 14:14:06 -0700

    Fix the unified log for Arduino ESP8266. The ESP8266 compiler don’t do a good job compiling this code, it do not understand that the ‘format’ is a ‘cont char*’ and moves it to the RAM as a global variable, increasing a lot the .bss. So, we create a specific LogInfo that explicitly pin the ‘format’ on the PROGMEM (flash) using a _localFORMAT variable with the macro PSTR.
        #define ICACHE_FLASH_ATTR   __attribute__((section(".irom0.text")))
        #define PROGMEM     ICACHE_RODATA_ATTR
        # define PSTR(s) (__extension__({static const char __c[] PROGMEM = (s); &__c[0];}))
        const char* __localFORMAT = PSTR(FORMAT);
    
    On the other hand, vsprintf do not support the pinned ‘format’ and os_printf do not works with va_list, so we compacted the log in the macro LogInfo.

--- src/consolelogger.c
+++ src/consolelogger.c
@@ -9,10 +9,31 @@
 #include <stdio.h>
 #include "azure_c_shared_utility/xlogging.h"
 
+#if defined _MSC_VER
+#define FUNC_NAME __FUNCDNAME__
+#else
+#define FUNC_NAME __func__
+#endif
+
 void consolelogger_log(LOG_CATEGORY log_category,unsigned int options, const char* format, ...)
 {
 	va_list args;
 	va_start(args, format);
+
+    time_t t = time(NULL); 
+    
+    switch (log_category)
+    {
+    case LOG_INFO:
+        (void)printf("Info: ");
+        break;
+    case LOG_ERROR:
+        (void)printf("Error: Time:%.24s File:%s Func:%s Line:%d ", ctime(&t), __FILE__, FUNC_NAME, __LINE__);
+        break;
+    default:
+        break;
+    }
+
 	(void)vprintf(format, args);
 	va_end(args);
 
