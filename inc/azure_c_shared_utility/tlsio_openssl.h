5477c739e96536fb02aaa2fcbd00c690a8a6a0a4
Marc Schier <marcschier@hotmail.com>
2016-09-20 09:01:00 +0200

    Make tlsio_*ssl.h mockable

--- inc/azure_c_shared_utility/tlsio_openssl.h
+++ inc/azure_c_shared_utility/tlsio_openssl.h
@@ -12,19 +12,20 @@ extern "C" {
 #endif /* __cplusplus */
 
 #include "azure_c_shared_utility/xio.h"
+#include "azure_c_shared_utility/umock_c_prod.h"
 
-extern int tlsio_openssl_init(void);
-extern void tlsio_openssl_deinit(void);
+MOCKABLE_FUNCTION(, int, tlsio_openssl_init);
+MOCKABLE_FUNCTION(, void, tlsio_openssl_deinit);
 
-extern CONCRETE_IO_HANDLE tlsio_openssl_create(void* io_create_parameters);
-extern void tlsio_openssl_destroy(CONCRETE_IO_HANDLE tls_io);
-extern int tlsio_openssl_open(CONCRETE_IO_HANDLE tls_io, ON_IO_OPEN_COMPLETE on_io_open_complete, void* on_io_open_complete_context, ON_BYTES_RECEIVED on_bytes_received, void* on_bytes_received_context, ON_IO_ERROR on_io_error, void* on_io_error_context);
-extern int tlsio_openssl_close(CONCRETE_IO_HANDLE tls_io, ON_IO_CLOSE_COMPLETE on_io_close_complete, void* callback_context);
-extern int tlsio_openssl_send(CONCRETE_IO_HANDLE tls_io, const void* buffer, size_t size, ON_SEND_COMPLETE on_send_complete, void* callback_context);
-extern void tlsio_openssl_dowork(CONCRETE_IO_HANDLE tls_io);
-extern int tlsio_openssl_setoption(CONCRETE_IO_HANDLE tls_io, const char* optionName, const void* value);
+MOCKABLE_FUNCTION(, CONCRETE_IO_HANDLE, tlsio_openssl_create, void*, io_create_parameters);
+MOCKABLE_FUNCTION(, void, tlsio_openssl_destroy, CONCRETE_IO_HANDLE, tls_io);
+MOCKABLE_FUNCTION(, int, tlsio_openssl_open, CONCRETE_IO_HANDLE, tls_io, ON_IO_OPEN_COMPLETE, on_io_open_complete, void*, on_io_open_complete_context, ON_BYTES_RECEIVED, on_bytes_received, void*, on_bytes_received_context, ON_IO_ERROR, on_io_error, void*, on_io_error_context);
+MOCKABLE_FUNCTION(, int, tlsio_openssl_close, CONCRETE_IO_HANDLE, tls_io, ON_IO_CLOSE_COMPLETE, on_io_close_complete, void*, callback_context);
+MOCKABLE_FUNCTION(, int, tlsio_openssl_send, CONCRETE_IO_HANDLE, tls_io, const void*, buffer, size_t, size, ON_SEND_COMPLETE, on_send_complete, void*, callback_context);
+MOCKABLE_FUNCTION(, void, tlsio_openssl_dowork, CONCRETE_IO_HANDLE, tls_io);
+MOCKABLE_FUNCTION(, int, tlsio_openssl_setoption, CONCRETE_IO_HANDLE, tls_io, const char*, optionName, const void*, value);
 
-extern const IO_INTERFACE_DESCRIPTION* tlsio_openssl_get_interface_description(void);
+MOCKABLE_FUNCTION(, const IO_INTERFACE_DESCRIPTION*, tlsio_openssl_get_interface_description);
 
 #ifdef __cplusplus
 }
