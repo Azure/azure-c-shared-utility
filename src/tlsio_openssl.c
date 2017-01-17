16145bf50f8af400ccb8c31346cf4e07353674b6
Marc Schier <marcschier@hotmail.com>
2017-01-17 10:45:23 +0100

    Fix merge issue

--- src/tlsio_openssl.c
+++ src/tlsio_openssl.c
@@ -844,53 +844,34 @@ static int create_openssl_instance(TLS_IO_INSTANCE* tlsInstance)
             }
             else
             {
-                const IO_INTERFACE_DESCRIPTION* underlying_io_interface = socketio_get_interface_description();
-                if (underlying_io_interface == NULL)
+                if ((BIO_set_mem_eof_return(tlsInstance->in_bio, -1) <= 0) ||
+                    (BIO_set_mem_eof_return(tlsInstance->out_bio, -1) <= 0))
                 {
                     (void)BIO_free(tlsInstance->in_bio);
                     (void)BIO_free(tlsInstance->out_bio);
                     SSL_CTX_free(tlsInstance->ssl_context);
                     tlsInstance->ssl_context = NULL;
                     result = __LINE__;
-                    LogError("Failed getting socket IO interface description.");
+                    LogError("Failed BIO_set_mem_eof_return.");
                 }
                 else
                 {
-                    SOCKETIO_CONFIG socketio_config;
-                    socketio_config.hostname = tlsInstance->hostname;
-                    socketio_config.port = tlsInstance->port;
-                    socketio_config.accepted_socket = NULL;
+                    SSL_CTX_set_verify(tlsInstance->ssl_context, SSL_VERIFY_PEER, NULL);
 
-                    tlsInstance->underlying_io = xio_create(underlying_io_interface, &socketio_config);
-                    if ((tlsInstance->underlying_io == NULL) ||
-                        (BIO_set_mem_eof_return(tlsInstance->in_bio, -1) <= 0) ||
-                        (BIO_set_mem_eof_return(tlsInstance->out_bio, -1) <= 0))
+                    // Specifies that the default locations for which CA certificates are loaded should be used.
+                    if (SSL_CTX_set_default_verify_paths(tlsInstance->ssl_context) != 1)
+                    {
+                        // This is only a warning to the user. They can still specify the certificate via SetOption.
+                        LogInfo("WARNING: Unable to specify the default location for CA certificates on this platform.");
+                    }
+
+                    tlsInstance->ssl = SSL_new(tlsInstance->ssl_context);
+                    if (tlsInstance->ssl == NULL)
                     {
                         (void)BIO_free(tlsInstance->in_bio);
                         (void)BIO_free(tlsInstance->out_bio);
                         SSL_CTX_free(tlsInstance->ssl_context);
                         tlsInstance->ssl_context = NULL;
-                        result = __LINE__;
-                        LogError("Failed xio_create.");
-                    }
-                    else
-                    {
-                        SSL_CTX_set_verify(tlsInstance->ssl_context, SSL_VERIFY_PEER, NULL);
-
-                        // Specifies that the default locations for which CA certificates are loaded should be used.
-                        if (SSL_CTX_set_default_verify_paths(tlsInstance->ssl_context) != 1)
-                        {
-                            // This is only a warning to the user. They can still specify the certificate via SetOption.
-                            LogInfo("WARNING: Unable to specify the default location for CA certificates on this platform.");
-                        }
-
-                        tlsInstance->ssl = SSL_new(tlsInstance->ssl_context);
-                        if (tlsInstance->ssl == NULL)
-                        {
-                            (void)BIO_free(tlsInstance->in_bio);
-                            (void)BIO_free(tlsInstance->out_bio);
-                            SSL_CTX_free(tlsInstance->ssl_context);
-                            tlsInstance->ssl_context = NULL;
                             log_ERR_get_error("Failed creating OpenSSL instance.");
                             result = __LINE__;
                         }
