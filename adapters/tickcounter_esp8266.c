4f2f635b05ad844c6515b1cf4db54abf574bce79
Rita Zhang <rita.z.zhang@gmail.com>
2017-01-20 20:26:59 -0800

    Fix issue with current_time implementation

--- adapters/tickcounter_esp8266.c
+++ adapters/tickcounter_esp8266.c
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft. All rights reserved.
+// Copyright (C) Firmwave Ltd., All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <stdlib.h>
@@ -13,37 +13,17 @@
 #include "azure_c_shared_utility/tickcounter.h"
 #include "azure_c_shared_utility/xlogging.h"
 
-#define INVALID_TIME_VALUE      (time_t)(-1)
-
-int g_time_ms = 0;
-
 typedef struct TICK_COUNTER_INSTANCE_TAG
 {
-    time_t init_time_value;
-    uint64_t current_ms;
+    unsigned char dummy;
 } TICK_COUNTER_INSTANCE;
 
 TICK_COUNTER_HANDLE tickcounter_create(void)
 {
     TICK_COUNTER_INSTANCE* result = (TICK_COUNTER_INSTANCE*)malloc(sizeof(TICK_COUNTER_INSTANCE));
-    if (result != NULL)
+    if (result == NULL)
     {
-        result->init_time_value = 0;
-
-        if (result->init_time_value == INVALID_TIME_VALUE)
-        {
-            LogError("tickcounter failed: time return INVALID_TIME.");
-            free(result);
-            result = NULL;
-        }
-        else
-        {
-            result->current_ms = 0;
-        }
-    }
-    else
-    {
-        LogError("tickcounter failed: result is NULL.");
+        LogError("Failed creating tick counter");
     }
     return result;
 }
@@ -56,31 +36,21 @@ void tickcounter_destroy(TICK_COUNTER_HANDLE tick_counter)
     }
 }
 
-int tickcounter_get_current_ms(TICK_COUNTER_HANDLE tick_counter, tickcounter_ms_t* current_ms)
+int tickcounter_get_current_ms(TICK_COUNTER_HANDLE tick_counter, tickcounter_ms_t * current_ms)
 {
     int result;
 
-    g_time_ms += 1;
-
     if (tick_counter == NULL || current_ms == NULL)
     {
-        LogError("tickcounter failed: Invalid Arguments.");
+        LogError("tickcounter failed: Invalid Arguments.\r\n");
         result = __LINE__;
     }
     else
     {
-        time_t time_value = g_time_ms;
-        if (time_value == INVALID_TIME_VALUE)
-        {
-            result = __LINE__;
-        }
-        else
-        {
-            TICK_COUNTER_INSTANCE* tick_counter_instance = (TICK_COUNTER_INSTANCE*)tick_counter;
-            tick_counter_instance->current_ms = (uint64_t)(difftime(time_value, tick_counter_instance->init_time_value) * 1000);
-            *current_ms = tick_counter_instance->current_ms;
-            result = 0;
-        }
+        //Currently configTICK_RATE_HZ is set to 100 (100Hz); a tick is 10ms
+        *current_ms = (tickcounter_ms_t)xTaskGetTickCount()*10;
+        result = 0;
     }
+
     return result;
 }
