1faee98cfedd6895684f2f0d357e027c1ee4f3f8
dcristo <dcristo@THINGIE42>
2017-01-23 17:25:01 -0800

    Add __cplusplus #ifdef to vector_types.h

--- inc/azure_c_shared_utility/vector_types.h
+++ inc/azure_c_shared_utility/vector_types.h
@@ -4,8 +4,20 @@
 #ifndef VECTOR_TYPES_H
 #define VECTOR_TYPES_H
 
+#ifdef __cplusplus
+#include <cstdbool>
+extern "C"
+{
+#else
+#include <stdbool.h>
+#endif
+
 typedef struct VECTOR_TAG* VECTOR_HANDLE;
 
 typedef bool(*PREDICATE_FUNCTION)(const void* element, const void* value);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* VECTOR_TYPES_H */
