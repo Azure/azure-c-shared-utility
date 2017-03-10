8784f5fd21ad6488eabb8a0f41f34130c85ac8ec
Andrei Porumb <anporumb@microsoft.com>
2017-03-10 07:41:42 -0800

    use W for CreateSemaphoreW

--- adapters/lock_win32.c
+++ adapters/lock_win32.c
@@ -12,7 +12,7 @@ LOCK_HANDLE Lock_Init(void)
 {
     /* Codes_SRS_LOCK_10_002: [Lock_Init on success shall return a valid lock handle which should be a non NULL value] */
     /* Codes_SRS_LOCK_10_003: [Lock_Init on error shall return NULL ] */
-    HANDLE result = CreateSemaphore(NULL, 1, 1, NULL);
+    HANDLE result = CreateSemaphoreW(NULL, 1, 1, NULL);
     if (result == NULL)
     {
         LogError("CreateSemaphore failed.");
