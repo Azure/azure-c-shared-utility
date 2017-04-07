928aaebc5bc84689aa49df909edf7ff3998c0309
Rita Zhang <rita.z.zhang@gmail.com>
2017-04-07 11:20:07 -0700

    Add copyright headers and update thirdpartynotice

--- adapters/esp8266_mock.h
+++ adapters/esp8266_mock.h
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 #ifndef _ESP8266_MOCK_H_
 #define _ESP8266_MOCK_H_
 
