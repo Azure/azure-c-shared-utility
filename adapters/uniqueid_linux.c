856854e30d60146c2a1d249585e8039cacea230f
ewertons <ewertons@microsoft.com>
2017-09-08 14:50:06 -0700

    Revert "Add UniqueId_GetStringFromBytes to uniqueid module"
    This reverts commit 1c314e97bc5632c2d2dd0f5820a3d89fc989a2cd
    (UniqueId_GetStringFromBytes).

--- adapters/uniqueid_linux.c
+++ adapters/uniqueid_linux.c
@@ -33,36 +33,3 @@ UNIQUEID_RESULT UniqueId_Generate(char* uid, size_t len)
     }
     return result;
 }
-
-UNIQUEID_RESULT UniqueId_GetStringFromBytes(unsigned char* uid_bytes, size_t uuid_size, char* output_string)
-{
-    UNIQUEID_RESULT result;
-
-    // Codes_SRS_UNIQUEID_09_001: [ If `uid` or `output_string` are NULL, UniqueId_GetStringFromBytes shall return UNIQUEID_INVALID_ARG ]
-    // Codes_SRS_UNIQUEID_09_002: [ If `uid_size` is zero or not a multiple of two, UniqueId_GetStringFromBytes shall return UNIQUEID_INVALID_ARG ]
-    if (uid_bytes == NULL || uuid_size == 0 || ((uuid_size % 2) != 0) || output_string == NULL)
-    {
-        LogError("Invalid argument (uid=%p, uuid_size=%d, output_string=%p)", uid_bytes, uuid_size, output_string);
-        result = UNIQUEID_INVALID_ARG;
-    }
-    else
-    {
-        // Codes_SRS_UNIQUEID_09_003: [ `output_string` shall be filled according to RFC4122 using the byte sequence in `uid` ]
-        size_t i, j;
-        for (i = 0, j = 0; i < uuid_size; i++, j += 2)
-        {
-            (void)sprintf(output_string + j, "%02x", uid_bytes[i]);
-
-            if (i == 3 || i == 5 || i == 7 || i == 9)
-            {
-                output_string[j + 2] = '-';
-                j++;
-            }
-        }
-
-        // Codes_SRS_UNIQUEID_09_004: [ If no failures occur, UniqueId_Generate shall return UNIQUEID_OK ]  
-        result = UNIQUEID_OK;
-    }
-
-    return result;
-}
--- devdoc/uniqueid_requirements.md
+++ devdoc/uniqueid_requirements.md
@@ -14,7 +14,6 @@ The UniqueIDs module provides pseudo unique identifiers to the rest of the modul
     DEFINE_ENUM(UNIQUEID_RESULT, UNIQUEID_RESULT_VALUES)
 
     extern UNIQUEID_RESULT UniqueId_Generate(char* uid, size_t bufferSize);
-	extern UNIQUEID_RESULT UniqueId_GetStringFromBytes(unsigned char* uid, size_t uuid_size, char* output_string);
 ```
 ###  UniqueId_Generate
 ```C
@@ -27,16 +26,3 @@ extern UNIQUEID_RESULT UniqueId_Generate(char* uid, size_t len);
 **SRS_UNIQUEID_07_003: [** If len is less then 37 then UniqueId_Generate shall return UNIQUEID_INVALID_ARG **]**
 
 **SRS_UNIQUEID_07_004: [** If there is a failure for any reason the UniqueId_Generate shall return UNIQUEID_ERROR **]**  
-
-
-###  UniqueId_GetStringFromBytes
-```C
-extern UNIQUEID_RESULT UniqueId_GetStringFromBytes(unsigned char* uid, size_t uuid_size, char* output_string);
-```
-**SRS_UNIQUEID_09_001: [** If `uid` or `output_string` are NULL, UniqueId_GetStringFromBytes shall return UNIQUEID_INVALID_ARG **]**
-
-**SRS_UNIQUEID_09_002: [** If `uid_size` is zero or not a multiple of two, UniqueId_GetStringFromBytes shall return UNIQUEID_INVALID_ARG **]**
-
-**SRS_UNIQUEID_09_003: [** `output_string` shall be filled according to RFC4122 using the byte sequence in `uid` **]**
-
-**SRS_UNIQUEID_09_004: [** If no failures occur, UniqueId_Generate shall return UNIQUEID_OK **]**  
