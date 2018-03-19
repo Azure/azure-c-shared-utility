86e5bcb9e5438bcdc373800cf6499fc812f8f2b7
John Spaith <jspaith@windows.microsoft.com>
2018-03-19 13:18:36 -0700

    Fix socketio_config (indirect) reference outside its scope block

--- src/tlsio_wolfssl.c
+++ src/tlsio_wolfssl.c
@@ -608,6 +608,7 @@ CONCRETE_IO_HANDLE tlsio_wolfssl_create(void* io_create_parameters)
             }
             else
             {
+                SOCKETIO_CONFIG socketio_config;
                 const IO_INTERFACE_DESCRIPTION* underlying_io_interface;
                 void* io_interface_parameters;
 
@@ -618,8 +619,6 @@ CONCRETE_IO_HANDLE tlsio_wolfssl_create(void* io_create_parameters)
                 }
                 else
                 {
-                    SOCKETIO_CONFIG socketio_config;
-
                     socketio_config.hostname = tls_io_config->hostname;
                     socketio_config.port = tls_io_config->port;
                     socketio_config.accepted_socket = NULL;
