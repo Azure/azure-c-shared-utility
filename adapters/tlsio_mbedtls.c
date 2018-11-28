eaf108be994874667bbe7c2b4271ace4d94e7a61
Arthur Ma <arthma@microsoft.com>
2018-11-28 14:49:46 +0800

    remove MBED_TLS_DEBUG_ENABLE code block and other minor fixes

--- adapters/tlsio_mbedtls.c
+++ adapters/tlsio_mbedtls.c
@@ -6,6 +6,7 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "mbed_wait_api.h"
 #include "mbedtls/config.h"
 #include "mbedtls/debug.h"
 #include "mbedtls/ssl.h"
@@ -21,8 +22,6 @@
 #include "azure_c_shared_utility/socketio.h"
 #include "azure_c_shared_utility/crt_abstractions.h"
 #include "azure_c_shared_utility/shared_util_options.h"
-
-//#define MBED_TLS_DEBUG_ENABLE
 
 static const char *const OPTION_UNDERLYING_IO_OPTIONS = "underlying_io_options";
 
@@ -77,21 +76,10 @@ typedef enum TLS_STATE_TAG
     TLS_STATE_CLOSING,
 } TLS_STATE;
 
-// DEPRECATED: debug functions do not belong in the tree.
-#if defined(MBED_TLS_DEBUG_ENABLE)
-void mbedtls_debug(void *ctx, int level, const char *file, int line, const char *str)
-{
-    (void)ctx;
-    ((void)level);
-    printf("%s (%d): %s\r\n", file, line, str);
-}
-#endif
-
 static void indicate_error(TLS_IO_INSTANCE *tls_io_instance)
 {
     if ((tls_io_instance->tlsio_state == TLSIO_STATE_NOT_OPEN) || (tls_io_instance->tlsio_state == TLSIO_STATE_ERROR))
     {
-        // TODO: return here to avoid memory double free issue for version 1.1.28, not sure it's fixed now and need further investigation
         return;
     }
     tls_io_instance->tlsio_state = TLSIO_STATE_ERROR;
@@ -415,10 +403,6 @@ static void mbedtls_init(TLS_IO_INSTANCE *tls_io_instance)
     mbedtls_ssl_conf_rng(&tls_io_instance->config, mbedtls_ctr_drbg_random, &tls_io_instance->ctr_drbg);
     mbedtls_ssl_conf_authmode(&tls_io_instance->config, MBEDTLS_SSL_VERIFY_REQUIRED);
     mbedtls_ssl_conf_min_version(&tls_io_instance->config, MBEDTLS_SSL_MAJOR_VERSION_3, MBEDTLS_SSL_MINOR_VERSION_3); // v1.2
-#if defined(MBED_TLS_DEBUG_ENABLE)
-    mbedtls_ssl_conf_dbg(&tls_io_instance->config, mbedtls_debug, stdout);
-    mbedtls_debug_set_threshold(1);
-#endif
 
     mbedtls_ssl_init(&tls_io_instance->ssl);
     mbedtls_ssl_set_bio(&tls_io_instance->ssl, tls_io_instance, on_io_send, on_io_recv, NULL);
@@ -616,6 +600,10 @@ int tlsio_mbedtls_close(CONCRETE_IO_HANDLE tls_io, ON_IO_CLOSE_COMPLETE on_io_cl
                     mbedtls_ssl_close_notify(&tls_io_instance->ssl);
                     tls_io_instance->tls_status = TLS_STATE_CLOSING;
                 }
+                else
+                {
+                    result = 0;
+                }
             }
         }
     }
@@ -624,7 +612,7 @@ int tlsio_mbedtls_close(CONCRETE_IO_HANDLE tls_io, ON_IO_CLOSE_COMPLETE on_io_cl
 
 int tlsio_mbedtls_send(CONCRETE_IO_HANDLE tls_io, const void *buffer, size_t size, ON_SEND_COMPLETE on_send_complete, void *callback_context)
 {
-    int result;
+    int result = 0;
 
     if (tls_io == NULL || (buffer == NULL) || (size == 0))
     {
@@ -664,7 +652,7 @@ void tlsio_mbedtls_dowork(CONCRETE_IO_HANDLE tls_io)
         if (tls_io_instance->tlsio_state == TLSIO_STATE_OPENING_UNDERLYING_IO || tls_io_instance->tlsio_state == TLSIO_STATE_IN_HANDSHAKE || tls_io_instance->tlsio_state == TLSIO_STATE_OPEN)
         {
             decode_ssl_received_bytes(tls_io_instance);
-            // No need to call xio_dowork here because it's called in on_io_recv which is the callback function of decode_ssl_received_bytes
+            // Note: no need to call xio_dowork here because it's called in on_io_recv which is the callback function of decode_ssl_received_bytes
         }
     }
 }
@@ -672,7 +660,7 @@ void tlsio_mbedtls_dowork(CONCRETE_IO_HANDLE tls_io)
 /*this function will clone an option given by name and value*/
 static void *tlsio_mbedtls_CloneOption(const char *name, const void *value)
 {
-    void *result;
+    void *result = NULL;
     if (name == NULL || value == NULL)
     {
         LogError("invalid parameter detected: const char* name=%p, const void* value=%p", name, value);
@@ -809,7 +797,7 @@ int tlsio_mbedtls_setoption(CONCRETE_IO_HANDLE tls_io, const char *optionName, c
 
 OPTIONHANDLER_HANDLE tlsio_mbedtls_retrieveoptions(CONCRETE_IO_HANDLE handle)
 {
-    OPTIONHANDLER_HANDLE result;
+    OPTIONHANDLER_HANDLE result = NULL;
     if (handle == NULL)
     {
         LogError("invalid parameter detected: CONCRETE_IO_HANDLE handle=%p", handle);
