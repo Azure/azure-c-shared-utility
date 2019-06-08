93fc4d32b1d47d7a6607ef7eb30f447b5280681e
Dan Cristoloveanu <dcristo@microsoft.com>
2019-06-07 18:39:00 -0700

    Fix tests includes and remove VLD checks

--- tests/constbuffer_array_ut/main.c
+++ tests/constbuffer_array_ut/main.c
@@ -1,15 +1,11 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+#include <stddef.h>
 #include "testrunnerswitcher.h"
 
 int main(void)
 {
     size_t failedTestCount = 0;
     RUN_TEST_SUITE(constbuffer_array_unittests, failedTestCount);
-
-#ifdef VLD_OPT_REPORT_TO_STDOUT
-    failedTestCount = (failedTestCount>0)?failedTestCount:-(int)VLDGetLeaksCount();
-#endif
-
     return failedTestCount;
 }
