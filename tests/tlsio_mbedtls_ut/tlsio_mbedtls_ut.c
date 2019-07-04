58db559837270a42b3527a697e1c054cd47b6933
Dan Cristoloveanu <dcristo@microsoft.com>
2019-07-03 21:13:45 -0700

    Update dependencies

--- tests/tlsio_mbedtls_ut/tlsio_mbedtls_ut.c
+++ tests/tlsio_mbedtls_ut/tlsio_mbedtls_ut.c
@@ -293,7 +293,6 @@ BEGIN_TEST_SUITE(tlsio_mbedtls_ut)
     TEST_SUITE_INITIALIZE(suite_init)
     {
         int result;
-        TEST_INITIALIZE_MEMORY_DEBUG(g_dllByDll);
         g_testByTest = TEST_MUTEX_CREATE();
         ASSERT_IS_NOT_NULL(g_testByTest);
 
@@ -351,7 +350,6 @@ BEGIN_TEST_SUITE(tlsio_mbedtls_ut)
         umock_c_deinit();
 
         TEST_MUTEX_DESTROY(g_testByTest);
-        TEST_DEINITIALIZE_MEMORY_DEBUG(g_dllByDll);
     }
 
     TEST_FUNCTION_INITIALIZE(method_init)
