088a99caa91e1b110a3d891c916fc30c2bf27316
jebrando <jelani.brandon@microsoft.com>
2019-08-05 13:19:35 -0700

    Update test file for failures

--- tests/socket_async_ut/socket_async_ut.c
+++ tests/socket_async_ut/socket_async_ut.c
@@ -1,6 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#ifdef WIN32
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #ifdef __cplusplus
 #include <cstdlib>
 #else
