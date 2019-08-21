780f47c50a718faf6f5780b9a77bd7c03d8cc466
Matt Durak <mdurak@microsoft.com>
2019-08-21 15:45:55 -0700

    Support reals for vsprintf_char and vsprintf_wchar

--- inc/azure_c_shared_utility/string_utils.h
+++ inc/azure_c_shared_utility/string_utils.h
@@ -26,21 +26,22 @@
 extern "C" {
 #endif
 
-/*produces a string as if printed by vprintf*/
-MOCKABLE_FUNCTION(,char*, vsprintf_char, const char *, format, va_list, va);
-
 /*produces a string as if printed by printf*/
 extern char* sprintf_char_function(const char* format, ...);
 
 /*produces a string as if printed by printf (will also verify arguments)*/
 #define sprintf_char(format, ...) (0?printf((format), __VA_ARGS__):0, sprintf_char_function((format), __VA_ARGS__))
 
-/*produces a string as if printed by vwprintf*/
-MOCKABLE_FUNCTION(, wchar_t*, vsprintf_wchar, const wchar_t*, format, va_list, va);
-
 /*produces a string as if printed by wprintf*/
 extern wchar_t* sprintf_wchar_function(const wchar_t* format, ...);
 #define sprintf_wchar(format, ...) (0?wprintf((format), __VA_ARGS__):0, sprintf_wchar_function((format), __VA_ARGS__))
+
+MOCKABLE_INTERFACE(string_utils_printf,
+    /*produces a string as if printed by vprintf*/
+    FUNCTION(, char*, vsprintf_char, const char*, format, va_list, va),
+    /*produces a string as if printed by vwprintf*/
+    FUNCTION(, wchar_t*, vsprintf_wchar, const wchar_t*, format, va_list, va)
+)
 
 /*
 below macros can be used with printf. example:
