9a0cf850ab2faaa90656379b0d62510192ba8f9f
Amar Sagare <amarsa@microsoft.com>
2019-08-22 09:06:06 -0700

    add a method for getting elapsed time in ms

--- adapters/timer.c
+++ adapters/timer.c
@@ -60,6 +60,23 @@ double timer_get_elapsed(TIMER_HANDLE timer)
     return result;
 }
 
+double timer_get_elapsed_ms(TIMER_HANDLE timer)
+{
+    double result;
+    if (timer == NULL)
+    {
+        LogError("invalid arg TIMER_HANDLE timer=%p", timer);
+        result = -1.0;
+    }
+    else
+    {
+        LARGE_INTEGER stopTime;
+        (void)QueryPerformanceCounter(&stopTime);
+        result = ((double)(stopTime.QuadPart - timer->startTime.QuadPart) / (double)timer->freq.QuadPart) * 1000;
+    }
+    return result;
+}
+
 void timer_destroy(TIMER_HANDLE timer)
 {
     if (timer == NULL)
