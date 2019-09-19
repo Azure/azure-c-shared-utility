04c29c10618bd8b09693777873f02ba7af947be4
Rajeev Massand <massand@gmail.com>
2019-09-19 11:09:40 -0700

    PR fix

--- src/dns_resolver_ares.c
+++ src/dns_resolver_ares.c
@@ -18,13 +18,8 @@
 #include "ares.h"
 
 // EXTRACT_IPV4 pulls the uint32_t IPv4 address out of an addrinfo struct
-// This will not be needed for the asynchronous design
-#ifdef WIN32_NOT_USED
-//#define EXTRACT_IPV4(ptr) ((struct sockaddr_in *) ptr->ai_addr)->sin_addr.S_un.S_addr
-#else
 // The default definition handles lwIP. Please add comments for other systems tested.
 #define EXTRACT_IPV4(ptr) ((struct sockaddr_in *) ptr->ai_addr)->sin_addr.s_addr
-#endif
 
 typedef struct
 {
