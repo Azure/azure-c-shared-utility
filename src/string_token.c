2ca13900e221f5fe18518ecc44996c1b95cc701c
Eric Wolz <ericwol@microsoft.com>
2021-12-06 17:28:52 -0800

    fixed string token

--- src/string_token.c
+++ src/string_token.c
@@ -47,13 +47,6 @@ static size_t* get_delimiters_lengths(const char** delimiters, size_t n_delims)
                 result = NULL;
                 break;
             }
-            else if (((i+1) * sizeof(size_t)) > malloc_size)
-            {
-                LogError("buffer overflow");
-                free(result);
-                result = NULL;
-                break;
-            }
             else 
             {
                 result[i] = strlen(delimiters[i]);
