ed4414e67fbf3c264846caedab99db3a7c20e9d6
Eric Wolz <ericwol@microsoft.com>
2024-01-23 17:16:39 -0800

    fix build break

--- adapters/x509_schannel.c
+++ adapters/x509_schannel.c
@@ -165,7 +165,7 @@ static int set_ecc_certificate_info(X509_SCHANNEL_HANDLE_DATA* x509_handle, unsi
     BYTE* pubKeyBuf = pPubKeyBlob->pbData + 1;
     BYTE* privKeyBuf = pPrivKeyInfo->PrivateKey.pbData;
 
-    if ((keyBlobSize == SIZE_MAX ||
+    if (keyBlobSize == SIZE_MAX ||
         (pKeyBlob = (BCRYPT_ECCKEY_BLOB*)malloc(keyBlobSize)) == NULL)
     {
         /*Codes_SRS_X509_SCHANNEL_02_010: [ Otherwise, x509_schannel_create shall fail and return a NULL X509_SCHANNEL_HANDLE. ]*/
