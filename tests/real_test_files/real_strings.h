92d26131ad0fa69a76ce24a8481165fe94ffaefc
Eric Wolz <ericwol@microsoft.com>
2024-03-08 13:30:39 -0800

    update REGISTER_GLOBAL_MOCK_HOOK

--- tests/real_test_files/real_strings.h
+++ tests/real_test_files/real_strings.h
@@ -33,6 +33,7 @@
     REGISTER_GLOBAL_MOCK_HOOK(STRING_copy_n, real_STRING_copy_n); \
     REGISTER_GLOBAL_MOCK_FAIL_RETURN(STRING_copy_n, __LINE__); \
     REGISTER_GLOBAL_MOCK_HOOK(STRING_c_str, real_STRING_c_str); \
+    REGISTER_GLOBAL_MOCK_HOOK(STRING_c_str, real_STRING_c_str_null_string); \
     REGISTER_GLOBAL_MOCK_HOOK(STRING_empty, real_STRING_empty); \
     REGISTER_GLOBAL_MOCK_FAIL_RETURN(STRING_empty, __LINE__); \
     REGISTER_GLOBAL_MOCK_HOOK(STRING_length, real_STRING_length); \
@@ -55,6 +56,7 @@
 #define STRING_copy                     real_STRING_copy
 #define STRING_copy_n                   real_STRING_copy_n
 #define STRING_c_str                    real_STRING_c_str
+#define STRING_c_str_null_string        real_STRING_c_str_null_string
 #define STRING_empty                    real_STRING_empty
 #define STRING_length                   real_STRING_length
 #define STRING_compare                  real_STRING_compare
